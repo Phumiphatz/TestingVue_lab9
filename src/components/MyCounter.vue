1056ada271f91b707c254633d00cc1dbf4f5a6bd
phumiphatz <demopumiphat@gmail.com>
2020-03-08 13:44:22 +0700

    finish 8

---
+++ src/components/MyCounter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1><slot></slot></h1>
+    <button @click="dec"><slot name="dec"></slot></button>
+    {{ number }}
+    <button @click="inc"><slot name="inc"></slot></button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['begin'],
+  data () {
+    return {
+      number: 0
+    }
+  },
+  methods: {
+    dec () {
+      this.number--
+      this.$emit('dec', this.number)
+    },
+    inc () {
+      this.number++
+      this.$emit('inc', this.number)
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ src/views/Counter.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <Mycounter begin="1" @inc="inc" @dec="dec">
+      Counter ของฉัน
+      <template v-slot:dec>
+        ลบ
+      </template>
+      <template v-slot:inc>
+        บวก
+      </template>
+    </Mycounter>
+  </div>
+</template>
+
+<script>
+import Mycounter from '../components/MyCounter'
+export default {
+  components: {
+    Mycounter
+  },
+  methods: {
+    inc (number) {
+      console.log('Counter View inc' + number)
+    },
+    dec (number) {
+      console.log('Counter View dec' + number)
+    }
+  }
+}
+</script>
+
+<style></style>
